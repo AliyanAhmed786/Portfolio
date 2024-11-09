00e0dd8abf9133e20c1a4999a4e9c85e4115335e
Aliyan Ahmed <e.aliyan2001@gmail.com>
2024-11-09 23:20:08 +0500

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
+// next.config.js
 module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
-  basePath: '/portfolio-assignment',  // Set the base path for GitHub Pages
-  assetPrefix: '/portfolio-assignment', // Prefix for static assets (JS, CSS, etc.)
+  
 };
